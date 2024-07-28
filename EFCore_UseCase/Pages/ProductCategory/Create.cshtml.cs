d58ce4f08898aacc3d52b440318f07a43f897158
Mahdi Morovati <morovati155@gmail.com>
2024-07-28 08:59:28 +0330

    create ProductCategory

---
+++ EFCore_UseCase/Pages/ProductCategory/Create.cshtml.cs
@@ -0,0 +1,26 @@
+using EfCore.Application.Contracts.ProductCategory;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EFCore_UseCase.Pages.ProductCategory;
+
+public class CreateModel : PageModel
+{
+    protected readonly IProductCategoryApplication _productCategoryApplication;
+
+    public CreateModel(IProductCategoryApplication productCategoryApplication)
+    {
+        _productCategoryApplication = productCategoryApplication;
+    }
+
+    public void OnGet()
+    {
+        
+    }
+
+    public RedirectToPageResult OnPost(CreateProductCategory command)
+    {
+        _productCategoryApplication.Create(command);
+        return RedirectToPage("./Index");
+    }
+}
